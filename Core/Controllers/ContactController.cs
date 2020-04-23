1df64ace6aaedda6b59500cc769bea87199a16d3
tungnh <tungnh@DESKTOP-CCRG921>
2020-04-23 17:43:10 +0700

    save

---
+++ Core/Controllers/ContactController.cs
@@ -0,0 +1,48 @@
+﻿using Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Core.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        [HttpGet]
+        public ActionResult RenderForm()
+        {
+            var model = new ContactViewModel()
+            {
+                ContactFormId = CurrentPage.Id,
+            };
+
+            return PartialView("/Views/Partials/Contact.contactForm.cshtml", model);
+        }
+
+        [HttpPost]
+        public ActionResult RenderForm(ContactViewModel model)
+        {
+            return PartialView("/Views/Partials/Contact.contactForm.cshtml", model);
+        }
+
+        public ActionResult SubmitForm(ContactViewModel model)
+        {
+            bool success = false;
+            if (ModelState.IsValid)
+            {
+                success = SendEmail();
+            }
+
+            var contactPage = UmbracoContext.Content.GetById(false, model.ContactFormId);
+
+        }
+
+        public bool SendEmail()
+        {
+            return false;
+        }
+    }
+}
---
+++ Core/ViewModels/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "You must enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Must enter a message")]
+        [MaxLength(500, ErrorMessage ="Max 500 Characters allowed")]
+        public string Message { get; set; }
+        public int ContactFormId { get; set; }
+    }
+}
